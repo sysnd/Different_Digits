e0907ef317d94182f157287ef6cfd8fc41c37d31
nia.dryankova <nia.dryankova@scalefocus.com>
2018-10-01 09:57:33 +0300

    Initial version of the code. Will be optimised.

---
+++ README.md
@@ -0,0 +1,14 @@
+"# Different_Digits"
+
+See this link for more info in Bulgarian:
+http://www.math.bas.bg/infos/2018_Sofia_shkola/C_day1/2_difdigits.pdf
+
+Simple explanation in English:
+
+Input: an integer n
+Output: an integer, which is the smallest integer larger than n that doesn't contain repetitive digits
+Limits: 1<=n<=10^18
+
+Example:
+Input: 98
+Output: 101
---
+++ difdigits/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace difdigits
+{
+    class Program
+    {
+        static bool isMatch = false;
+        static long numberResult;
+        static int count = 0;
+        static Stopwatch watch = new Stopwatch();
+        static void Main(string[] args)
+        {
+            watch.Start();
+            long n = long.Parse(Console.ReadLine());
+            long number = n + 1;
+            Calculate(number);
+            Console.WriteLine(watch.ElapsedMilliseconds.ToString());
+            watch.Stop();
+        }
+        static int Calculate(long number)
+        {
+            long localNumber = number;
+            long originalNumber = localNumber;
+            int length = localNumber.ToString().Length;
+            while (!isMatch)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    int lastDigit = (int)localNumber % 10;
+                    int previousDigit = (int)(localNumber / 10) % 10;
+                    if (lastDigit == previousDigit)
+                    {
+                        originalNumber++;
+                        Calculate(originalNumber);
+
+                    }
+                    else
+                    {
+                        localNumber /= 10;
+                        length--;
+                        continue;
+                    }
+                }
+                isMatch = true;
+                if (++count == 1)
+                {
+                    numberResult = originalNumber;
+                    Console.WriteLine(numberResult);
+                }
+            }
+                return 0;
+           
+        }
+    }
+}
